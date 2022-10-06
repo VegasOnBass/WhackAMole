d763a66beba944c8cfb3071aaded9dc673bfc34e
“Feggens <fmonbrun@yahoo.com>
2022-10-05 23:21:54 -0400

    make it better

--- server.js
+++ server.js
@@ -24,6 +24,12 @@ app.use((err, req, res, next) => {
   next();
 });
 
+if (process.env.NODE_ENV === "production" || process.env.NODE_ENV === "staging") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
+  });
+ }
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
